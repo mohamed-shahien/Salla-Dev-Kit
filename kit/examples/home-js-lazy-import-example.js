bec4939d970cae7bb8e263eb0e919434ce21e98f
mohamed-shahien <shaheenagain@gmail.com>
2026-05-31 05:46:17 +0300

    feat: implement CLI for project initialization and diagnostic checks with support for selective file copying

--- kit/examples/home-js-lazy-import-example.js
+++
@@ -1,6 +0,0 @@
-lazyImportWhenExists({
-  key: "F__example_component",
-  selector: ".F__exampleComponent",
-  importer: () => import("./services/home/F__example_component"),
-  init: (mod, hosts) => mod.default(hosts, helpers),
-});
